e3c2ab9b04a5cac06c4e6c3b83718df515ad5e86
xaocho0987 <63099187+xaocho0987@users.noreply.github.com>
2023-05-16 01:38:42 +0700

    up

---
+++ admin/pages-403.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html
+        lang="en"
+        class="light-style"
+        dir="ltr"
+        data-theme="theme-default"
+        data-assets-path="../assets/"
+        data-template="vertical-menu-template-free"
+>
+<head>
+    <meta charset="utf-8"/>
+    <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
+    />
+
+    <title>Truy cập bị từ chối🙅</title>
+    
+    <meta name="description" content=""/>
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="../admin/resource/assets/img/favicon/favicon.ico"/>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+    <link
+            href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+            rel="stylesheet"
+    />
+
+    <!-- Icons. Uncomment required icon fonts -->
+    <link rel="stylesheet" href="../admin/resource/assets/vendor/fonts/boxicons.css"/>
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="../admin/resource/assets/vendor/css/core.css" class="template-customizer-core-css"/>
+    <link rel="stylesheet" href="../admin/resource/assets/vendor/css/theme-default.css" class="template-customizer-theme-css"/>
+    <link rel="stylesheet" href="../admin/resource/assets/css/demo.css"/>
+
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="../admin/resource/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css"/>
+
+    <!-- Page CSS -->
+    <!-- Page -->
+    <link rel="stylesheet" href="../admin/resource/assets/vendor/css/pages/page-misc.css"/>
+    <!-- Helpers -->
+    <script src="../admin/resource/assets/vendor/js/helpers.js"></script>
+
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="../admin/resource/assets/js/config.js"></script>
+</head>
+
+<body>
+<!-- Content -->
+
+<!--Under Maintenance -->
+<div class="container-xxl container-p-y">
+    <div class="misc-wrapper">
+        <h2 class="mb-2 mx-2">Truy cập bị từ chối🙅</h2>
+        <p class="mb-4 mx-2">Sorry, bạn cần có quyền truy cập. Hãy yêu cầu quyền truy cập hoặc chuyển sang tài khoản có quyền!!!</p>
+        <a href="index.php" class="btn btn-primary">Về lại trang chủ</a>
+        <div class="mt-4">
+            <img
+                    src="../admin/resource/assets/img/illustrations/403 Error Forbidden-rafiki.png"
+                    alt="403 Error Forbidden-rafiki"
+                    width="500"
+                    class="img-fluid"
+                    data-app-dark-img="illustrations/girl-doing-yoga-dark.png"
+                    data-app-light-img="illustrations/403 Error Forbidden-rafiki.png"
+            />
+        </div>
+    </div>
+</div>
+<!-- /Under Maintenance -->
+
+<!-- / Content -->
+
+<!-- Core JS -->
+<!-- build:js assets/vendor/js/core.js -->
+<script src="../admin/resource/assets/vendor/libs/jquery/jquery.js"></script>
+<script src="../admin/resource/assets/vendor/libs/popper/popper.js"></script>
+<script src="../admin/resource/assets/vendor/js/bootstrap.js"></script>
+<script src="../admin/resource/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+<script src="../admin/resource/assets/vendor/js/menu.js"></script>
+<!-- endbuild -->
+
+<!-- Vendors JS -->
+
+<!-- Main JS -->
+<script src="../admin/resource/assets/js/main.js"></script>
+
+<!-- Page JS -->
+</body>
+</html>
